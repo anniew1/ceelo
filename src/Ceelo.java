4af4fb673711851ebf6224cd5e868ddd4b0b7bf4
BT_1N3_08 <dwa@bths.edu>
2024-01-05 08:43:58 -0500

    added wagering

--- src/Ceelo.java
+++ src/Ceelo.java
@@ -5,8 +5,10 @@ public class Ceelo {
     private Player p1;
     private Player p2;
     private Player p3;
+    private Banker banker;
     public Ceelo() {
         scan = new Scanner(System.in);
+        banker = new Banker();
     }
 
     public void runGame() {
@@ -15,6 +17,31 @@ public class Ceelo {
 
     private void gameLogic() {
         welcome();
+        while ((p1.getChips() != 0 && p2.getChips() != 0 && p3.getChips() != 0) || banker.getChips() != 0) {
+            printStatus();
+            System.out.println();
+            makeWager(p1.getOutOfGame(), p1);
+            makeWager(p2.getOutOfGame(), p2);
+            makeWager(p3.getOutOfGame(), p3);
+        }
+    }
+
+    // welcomes the players into the game and explains the rules and also initializes all three players
+    private void welcome() {
+        ConsoleUtility.clearScreen();
+        System.out.println("Welcome to Cee - lo!");
+        System.out.println();
+        System.out.println("Rules: ");
+        System.out.println("There are 3 players and a banker (controlled by computer)");
+        System.out.println("The goal of the game is to try to bankrupt the banker before he takes all your chips.");
+        System.out.println("If the players successfully bankrupt the banker, the player with the most chips wins!");
+
+        try {
+            Thread.sleep(10000);
+        } catch (Exception e) {
+            System.out.println("error");
+        }
+        ConsoleUtility.clearScreen();
 
         // asks the players for their names and creates the three player objects
         System.out.print("Player 1, enter your name: ");
@@ -25,29 +52,39 @@ public class Ceelo {
 
         System.out.print("Player 3, enter your name: ");
         p3 = new Player(scan.nextLine());
-    }
 
-    // welcomes the players into the game and explains the rules
-    private void welcome() {
-        System.out.println("Welcome to Cee - lo!");
-        System.out.println("Rules: ");
-        System.out.println("There are 3 players and a banker (controlled by computer)");
-        System.out.println("The goal of the game is to try to bankrupt the banker before he takes all your chips.");
-        System.out.println("If the players successfully bankrupt the banker, the player with the most chips wins!");
-        try {
-            Thread.sleep(10000);
-        } catch (Exception e) {
-            System.out.println("error");
-        }
         ConsoleUtility.clearScreen();
-        System.out.println();
-
     }
 
+    // prints out how many chips each player and banker has each round
     private void printStatus(){
         System.out.println(p1.getName() + " has " + p1.getChips() + " chips");
         System.out.println(p2.getName() + " has " + p2.getChips() + " chips");
         System.out.println(p3.getName() + " has " + p3.getChips() + " chips");
+        System.out.println("The banker has " + banker.getChips() + " chips");
     }
+
+
+    // asks the player how much they would like to wager
+    private void makeWager(Boolean playerout, Player player) {
+        if (!playerout){
+            System.out.println(player.getName() + "'s Turn:");
+            System.out.print("How much would you like to wager (0 -" + player.getChips() + "):" );
+            player.setCurrentWager(scan.nextInt());
+            while (player.getCurrentWager() < 0 || player.getCurrentWager() > player.getChips()) {
+                System.out.println("Invalid wager. ");
+                System.out.print("How much would you like to wager (0 -" + player.getChips() + "):" );
+                player.setCurrentWager(scan.nextInt());
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                System.out.println("error");
+            }
+
+            ConsoleUtility.clearScreen();
+        }
+    }
+
 
 }
--- src/Player.java
+++ src/Player.java
@@ -3,9 +3,15 @@ public class Player {
     private int chips;
     private String name;
 
+    private int currentWager;
+
+    private Boolean outOfGame;
+
     public Player(String name) {
         chips = 100;
         this.name = name;
+        currentWager = 0;
+        outOfGame = false;
     }
 
     // returns the number of chips player has
@@ -18,8 +24,23 @@ public class Player {
         return name;
     }
 
+    // returns a boolean value indicating whether the player is out of the game
+    public Boolean getOutOfGame() {
+        return outOfGame;
+    }
+
+    // returns the amount that the player wagered in that round
+    public int getCurrentWager() {
+        return currentWager;
+    }
+
+    public void setCurrentWager(int wager) {
+        currentWager = wager;
+    }
+
     // changes the amount of chips player has after round
     public void changeChips(int difference) {
         chips += difference;
     }
+
 }
